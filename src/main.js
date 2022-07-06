d22d11557612ddfbda7243f9cc8274c91ff703c5
JuliaKirik <juliakirichok9878@gmail.com>
2022-07-06 23:03:27 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-//$('.test').css('color', 'red');
-//$('h2.test').css('color', 'red');
-//$('p + p').css('color', 'red');
+$('.test').css('color', 'red');
+$('h2.test').css('color', 'red');
+$('p + p').css('color', 'red');
 $('li.www + li').css('color', 'red');
-//$('h2 i').css('color', 'red');
-//$('h2 > i').css('color', 'red');
+$('h2 i').css('color', 'red');
+$('h2 > i').css('color', 'red');
 
-// $(':header').css('color', 'red');
-// $('h2').not('.test').css('color', 'red');
-// $(':header').not('.test').css('color', 'red');
-// $(':header ~ :header').css('color', 'red');
-// $(':header.test + :header.test').css('color', 'red');
+$(':header').css('color', 'red');
+$('h2').not('.test').css('color', 'red');
+$(':header').not('.test').css('color', 'red');
+$(':header ~ :header').css('color', 'red');
+$(':header.test + :header.test').css('color', 'red');
 
